3dab3dcf428b170842bdf67cb8faf6dc4b3013b7
Nathaniel Starkman <nstarkman@protonmail.com>
2019-11-21 01:52:29 -0500

    PEP8 formatting
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,101 +1,130 @@
+# -*- coding: utf-8 -*-
+
+"""sitecustomize for autoinstallation.
+
+This script is to be installed in the Python installation's "sitecustomize.py"
+(which may or may not previously exist).
+
+To find the installation directory, enter this into a Python prompt:
+
+>>> import site
+>>> print(site.getusersitepackages())
+
+Open (or create) "sitecustomize.py" in this directory insert the code below.
+Some parts of the directory path may also need to be created.
+
+"""
+
+##############################################################################
+# IMPORTS
+
 import sys
 
-# This script is to be installed in the Python installation's "sitecustomize.py" file
-# (which may or may not previously exist).
-#
-# To find the directory where this file belongs, enter this into a Python prompt:
-#
-# import site
-# print(site.getusersitepackages())
-# 
-# Open (or create) "sitecustomize.py" in this directory and place the code below. Some
-# parts of the directory path may also need to be created.
-#
+
+##############################################################################
+# FUNCTION
 
 def handle_import_exception(exc_type, exc_value, exc_traceback):
+    """handle_import_exception.
 
-	if exc_type.__name__ == "ModuleNotFoundError":
-		import __main__
-		import subprocess
+    Change how ModuleNotFoundError behaves
+    Now ModuleNotFoundError will try to install the missing module.
 
-		# ---------------------
-		# Configuration Options
-		# ----------------------------------------------------------------------------------
-		# change this to set whether to ask to install a package or just attempt it directly
-		ask_to_install = False
-		
-		# set this to 'True' if you need to use 'sudo' to "pip install" packages
-		uses_sudo = True
-		# ----------------------------------------------------------------------------------
+    """
+    if exc_type.__name__ == "ModuleNotFoundError":
 
-		debug_mode = False
+        import __main__
+        import subprocess
+        import importlib
 
-		# Are we running in the interactive interpreter?
-		interactive_mode = not hasattr(__main__, '__file__')
-		
-		# get module name; exception string is in the form
-		# "No module named 'xxx'"
-		module_name = str(exc_value).split("'")[1]
+        # --------------------------------------------------------------------
+        # Configuration Options
 
-		if ask_to_install:
-			print(f"Your script requires the package '{module_name}', but you don't have it installed.")
-			ans = input("Would you like me to try to install it (y/n)? ")
-			if ans.lower() == 'y':
-				pass
-			else:
-				print("OK! Here's the command if you want to do it on your own:")
-				print("")
-				install_command = f"pip install {module_name}"
-				if uses_sudo:
-					print("sudo " + install_command)
-				else:
-					print(install_command)
-				print("")
-				sys.exit(0)
-		else:
-			print(f"\nThe package '{module_name}' is not installed... attempting installation.")
-		
-		commands = ["pip", "install", f"{module_name}"] # "--user" after 'install'
-		if uses_sudo:
-			commands.insert(0, "/usr/bin/sudo")
+        # whether to ask to install a package
+        ASK_TO_INSTALL = True
 
-		with subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-							  bufsize=1, universal_newlines=True) as p:
-			out, err = p.communicate()
+        # set to 'True' if you need to use 'sudo' to "pip install" packages
+        USES_SUDO = False
 
-		# debugging
-		if debug_mode:
-			print("--- stdout")
-			print(out)
-			print("--- stderr")
-			print(err)
-			print("---")
+        # --------------------------------------------------------------------
 
-		import importlib
+        debug_mode = False
 
-		if "ERROR: Could not find a version" in err or "ERROR: No matching distribution" in err:
-			print(f"Sorry, pip could not find a package named {module_name}.\n")
-			sys.exit(0)
-		
-		elif "Successfully installed" in out:
-			# should be installed now; try again
-			try:
-				module = importlib.import_module(f'{module_name}')
-			except ModuleNotFoundError:
-				print("\nSorry, the package installation failed. :(\n")
-				print("Here are the messages to help debug: ")
-				print(out)
-				print(err)
-				sys.exit(0)
-			
-			print(f"The package '{module_name}' was sucessfully installed; please rerun your script.\n")
-				
-		else:
-			print("\nSorry, the package installation failed. :(\n")
-			print("Here are the messages to help debug: ")
-			print(out)
-			print(err)
-			sys.exit(0)
-		
+        # Are we running in the interactive interpreter?
+        interactive_mode = not hasattr(__main__, '__file__')
+
+        # get module name; exception string is in the form
+        # "No module named 'xxx'"
+        module_name = str(exc_value).split("'")[1]
+
+        if ASK_TO_INSTALL:
+            print((f"Your script requires the package '{module_name}',"
+                   " but you don't have it installed."))
+
+            ans = input("Would you like me to try to install it (y/n)? ")
+            if ans.lower() == 'y':
+                pass
+            else:
+                print(("OK! Here's the command "
+                       "if you want to do it on your own:\n"))
+                install_command = f"pip install {module_name} --user"
+                if USES_SUDO:
+                    print("sudo " + install_command)
+                else:
+                    print(install_command, "", sep='\n')
+                sys.exit(0)
+        else:
+            print((f"\nThe package '{module_name}' is not installed..."
+                   " attempting installation."))
+        # /if
+
+        commands = ["pip", "install", f"{module_name}", "--user"]
+        # commands = ["conda", "install", f"{module_name}", '--yes']
+        if USES_SUDO:
+            commands.insert(0, "/usr/bin/sudo")
+
+        with subprocess.Popen(commands, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              bufsize=1, universal_newlines=True) as p:
+            out, err = p.communicate()
+
+        # debugging
+        if debug_mode:
+            print("--- stdout")
+            print(out)
+            print("--- stderr")
+            print(err)
+            print("---")
+
+        if (("ERROR: Could not find a version" in err) or
+                ("ERROR: No matching distribution" in err)):
+            print(
+                f"Sorry, pip could not find a package named {module_name}.\n")
+            sys.exit(0)
+
+        elif "Successfully installed" in out:
+            # should be installed now; try again
+            try:
+                importlib.import_module(f'{module_name}')
+            except ModuleNotFoundError:
+                print("\nSorry, the package installation failed. :(",
+                      "Here are the messages to help debug: ",
+                      out, err,
+                      sep='\n')
+                sys.exit(0)
+
+            print((f"The package '{module_name}' was sucessfully installed;"
+                   " please rerun your script.\n"))
+
+        else:
+            print("\nSorry, the package installation failed. :(",
+                  "Here are the messages to help debug: ",
+                  out, err,
+                  sep='\n')
+            sys.exit(0)
+
 sys.excepthook = handle_import_exception
+# /def  # needed by setup.py to locate end of code
 
+##############################################################################
+# END
